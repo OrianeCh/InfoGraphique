9829ba897d728fd39ce6721cc8c990e091e1e4d6
OrianeCh <oriane.charbonnel@ecl17.ec-lyon.fr>
2021-01-22 12:22:39 +0100

    Réorganisation

--- main.cpp
+++ main.cpp
@@ -56,9 +56,17 @@ Vector operator-(const Vector& a, const Vector& b)
 {
 	return Vector(a[0] - b[0], a[1] - b[1], a[2] - b[2]);
 }
+Vector operator-(const Vector& a) 
+{
+	return Vector(- a[0], - a[1], - a[2]);
+}
 double dot(const Vector& a, const Vector& b) 
 {
 	return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
+}
+double carre(double x) 
+{
+	return x * x;
 }
 
 
@@ -71,7 +79,7 @@ public:
 
 class Sphere {
 public:
-	explicit Sphere(const Vector& O, double R, const Vector& albedo) : O(O), R(R), albedo(albedo) {
+	explicit Sphere(const Vector& O, double R, const Vector& albedo, bool isMirror = false, bool isTransparent = false) : O(O), R(R), albedo(albedo), isMirror(isMirror), isTransparent(isTransparent) {
 	};
 	bool intersect(const Ray& r, Vector& P, Vector& N, double& t)
 	// renvoie true si le rayon r et la sphere s'intersectent
@@ -103,6 +111,7 @@ public:
 
 	};
 	Vector albedo;
+	bool isMirror, isTransparent;
 private :
 	Vector O;
 	double R;
@@ -111,7 +120,7 @@ private :
 class Scene {
 public:
 	Scene() {};
-	bool intersect(const Ray& r, Vector& P, Vector& N, Vector& albedo, double &t)
+	bool intersect(const Ray& r, Vector& P, Vector& N, Vector& albedo, bool &mirror, bool &transparent, double &t)
 	// return true si il y a intersection avec au moins une sphere de la scene
 	// donne le point P et la normale N de l'intersection la plus proche de l’origine du rayon 
 	// parmi les intersections avec toutes les sphères de la scène
@@ -130,12 +139,74 @@ public:
 				albedo = objects[i].albedo;
 				P = localP;
 				N = localN;
+				mirror = objects[i].isMirror;
+				transparent = objects[i].isTransparent;
 			}
 		}
 		return has_inter;
+	}
 
+	Vector get_color(const Ray& r, int rebond){
+
+		if (rebond > 5) return Vector(0.,0.,0.);
+
+		Vector P, N, albedo;
+		double t;
+		bool mirror, transparent;
+		bool inter = intersect(r, P, N, albedo, mirror, transparent, t);
+		Vector color(0, 0, 0);    // noir 
+		
+		if (inter) 
+		{
+			if (mirror) {
+				Vector reflectedDir = r.u - 2*dot(r.u, N)*N;
+				Ray reflectedRay(P + 0.001*N, reflectedDir);
+				return get_color(reflectedRay, rebond + 1);
+			} 
+			else {
+				if (transparent) {
+					Vector reflectedDir = r.u - 2*dot(r.u, N)*N;
+					Ray reflectedRay(P + 0.001*N, reflectedDir);
+					double n1 = 1, n2 = 1.4;
+					Vector N2 = N;
+					if (dot(r.u, N) > 0) {
+						std::swap(n1, n2);
+						N2 = -N;
+					}
+					Vector Tt = n1 / n2 * (r.u - dot(r.u, N2)*N2);
+					double rad = 1 - carre(n1/n2) * (1 - carre(dot(r.u, N2)));
+					if (rad < 0) {
+						Vector reflectedDir = r.u - 2*dot(r.u, N)*N;
+						Ray reflectedRay(P + 0.001*N, reflectedDir);
+						return get_color(reflectedRay, rebond + 1);
+					}
+					Vector Tn = - sqrt(rad)*N2;
+					Vector refractedDir = Tt + Tn;
+					return get_color(Ray(P - 0.001*N2, refractedDir), rebond + 1);	
+				}
+				else {
+					Vector PL = L-P;
+					double d = sqrt(PL.carreNorm());    
+					// distance entre le point d'intersection et la lumière
+					Vector shadowP, shadowN, shadowAlbedo;
+					double shadowt;
+					bool shadowMirror, shadowTransp;
+					Ray shadowRay(P + 0.001*N, PL/d);
+					bool ombre = intersect(shadowRay, shadowP, shadowN, shadowAlbedo, shadowMirror, shadowTransp, shadowt);
+					if (ombre && shadowt < d) {
+						color = Vector(0., 0., 0.);
+					} else {
+						color = I / (4*M_PI*d*d) * albedo/M_PI *std::max(0., dot(N, PL/d));
+					}
+				}
+				
+			}
+			return color;
+		}
 	}
 	std::vector<Sphere> objects;
+	Vector L;
+	double I;
 
 };
 
@@ -145,13 +216,13 @@ int main() {
 
 	Vector C(0, 0, 55);    // centre de la caméra
 	Scene scene;
-	Sphere S1(Vector(0, 0, 0), 10, Vector(1, 1, 1));
+	Sphere S1(Vector(0, 0, 0), 10, Vector(1, 1, 1), false, true);
 	Sphere Ssol(Vector(0, -1000, 0), 990, Vector(1, 1, 1));
 	Sphere Smur1(Vector(-1000, 0, 0), 940, Vector(1, 0., 0.));
 	Sphere Smur2(Vector(1000, 0, 0), 940, Vector(0., 1, 0.));
 	Sphere Smur3(Vector(0, 0, -1000), 940, Vector(0., 0., 1));
 	Sphere Smur4(Vector(0, 0, 1000), 940, Vector(0., 1, 1));
-	Sphere Splafond(Vector(0, 1000, 0), 940, Vector(1, 0., 0.));
+	Sphere Splafond(Vector(0, 1000, 0), 940, Vector(1, 0.5, 0.));
 	scene.objects.push_back(S1);
 	scene.objects.push_back(Ssol);
 	scene.objects.push_back(Smur1);
@@ -161,9 +232,9 @@ int main() {
 	scene.objects.push_back(Splafond);
 
 	double fov = 60 * M_PI /180;
-	double I = 5E9;    // Intensité de la source lumineuse
+	scene.I = 5E9;    // Intensité de la source lumineuse
 
-	Vector L(-10, 20, 40);    // Position de la source lumineuse
+	scene.L = Vector(-10, 20, 40);    // Position de la source lumineuse
 
 	std::vector<unsigned char> image(W*H * 3, 0);
 	// tableau dynamique que l'on remplit au fur et à mesure de la boucle
@@ -176,25 +247,9 @@ int main() {
 			// direction du rayon sortant par le pixel i,j de la caméra
 			u = u.get_normalized();
 			Ray r(C, u);
-			Vector P, N, albedo;
-			double t;
-			bool inter = scene.intersect(r, P, N, albedo, t);
-			Vector color(0, 0, 0);    // noir 
-			if (inter) 
-			{
-				Vector PL = L-P;
-				double d = sqrt(PL.carreNorm());    
-				// distance entre le point d'intersection et la lumière
-				Vector shadowP, shadowN, shadowAlbedo;
-				double shadowt;
-				Ray shadowRay(P + 0.001*N, PL/d);
-				bool ombre = scene.intersect(shadowRay, shadowP, shadowN, shadowAlbedo, shadowt);
-				if (ombre && shadowt < d) {
-					color = Vector(0., 0., 0.);
-				} else {
-					color = I / (4*M_PI*d*d) * albedo/M_PI *std::max(0., dot(N, PL/d));
-				}
-			};
+
+			Vector color = scene.get_color(r, 0);
+
 			image[((H-i-1)*W + j) * 3 + 0] = std::min(255., pow(color[0], 0.45));
 			image[((H-i-1)*W + j) * 3 + 1] = std::min(255., pow(color[1], 0.45));
 			image[((H-i-1)*W + j) * 3 + 2] = std::min(255., pow(color[2], 0.45));
